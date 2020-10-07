a4ee094624c5fcecb3e48cbbc216fa1c5ae8f913
LambdaXymox <lambda.xymox@gmail.com>
2020-10-07 16:02:10 -0500

    Add light space data to do calculations from the local coordinate frame of the lights as well.

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@ use cglinalg::{
     Vector3,
     Vector4,
     Matrix4x4,
+    InvertibleSquareMatrix,
     Quaternion,
     Radians,
     Degrees,
@@ -270,19 +271,65 @@ struct LightAttitude<S> {
     /// the axis of rotation of the light be the same as one of the coordinate
     /// axes.
     axis: Quaternion<S>,
+    /// The translation matrix mapping objects from the world space coordinate
+    /// frame to the coordinate frame centered at the eye position of the camera.
+    translation_matrix: Matrix4x4<S>,
+    /// The rotation matrix rotating the a vector in world space to the coordinate
+    /// system of the camera's view space.
+    rotation_matrix: Matrix4x4<S>,
+    /// The viewing matrix of the camera mapping the complete translation + rotation
+    /// of the camera.
+    view_matrix: Matrix4x4<S>,
 }
 
 impl<S> LightAttitude<S> where S: ScalarFloat {
     /// Construct the camera's viewing transformation from its specification. 
     #[inline]
     fn from_spec(spec: &LightAttitudeSpec<S>) -> Self {
+        let axis = Quaternion::from_parts(S::zero(), spec.axis);
+        let translation_matrix = Matrix4x4::from_affine_translation(
+            &(-spec.position)
+        );
+        let rotation_matrix = Matrix4x4::from(&axis);
+        let view_matrix = rotation_matrix * translation_matrix;
+
         Self {
             position: spec.position,
             forward: spec.forward.to_homogeneous(),
             right: spec.right.to_homogeneous(),
             up: spec.up.to_homogeneous(),
-            axis: Quaternion::from_parts(S::zero(), spec.axis),
+            axis: axis,
+            translation_matrix: translation_matrix,
+            rotation_matrix: rotation_matrix,
+            view_matrix: view_matrix,
         }
+    }
+
+    /// Get the camera's up direction in camera space.
+    #[inline]
+    fn up_axis_eye(&self) -> Vector3<S> {
+        let zero = S::zero();
+        let one = S::one();
+
+        Vector3::new(zero, one, zero)
+    }
+        
+    /// Get the camera's right axis in camera space.
+    #[inline]
+    fn right_axis_eye(&self) -> Vector3<S> {
+        let zero = S::zero();
+        let one = S::one();
+        
+        Vector3::new(one, zero ,zero)
+    }
+        
+    /// Get the camera's forward axis in camera space.
+    #[inline]
+    fn forward_axis_eye(&self) -> Vector3<S> {
+        let zero = S::zero();
+        let one = S::one();
+        
+        Vector3::new(zero, zero, -one)
     }
 
     /// Update the light position based on the change in the camera's 
@@ -292,6 +339,11 @@ impl<S> LightAttitude<S> where S: ScalarFloat {
         self.position += self.forward.contract() * -delta_attitude.delta_position.z;
         self.position += self.up.contract()      *  delta_attitude.delta_position.y;
         self.position += self.right.contract()   *  delta_attitude.delta_position.x;
+
+        let translation_inv = Matrix4x4::from_affine_translation(
+            &self.position
+        );
+        self.translation_matrix = translation_inv.inverse().unwrap();
     }
 
     /// Update the light axes so we can rotate the camera about the new rotation axes.
@@ -319,11 +371,17 @@ impl<S> LightAttitude<S> where S: ScalarFloat {
         self.forward = rotation_matrix_inv * Vector4::new(S::zero(), S::zero(), -S::one(), S::zero());
         self.right   = rotation_matrix_inv * Vector4::new(S::one(), S::zero(), S::zero(), S::zero());
         self.up      = rotation_matrix_inv * Vector4::new(S::zero(), S::one(), S::zero(), S::zero());
+        self.rotation_matrix = rotation_matrix_inv.inverse().unwrap();
     }
 
     #[inline]
     fn update_position_world(&mut self, new_position: &Vector3<S>) {
         self.position = new_position.clone();
+        let translation_inv = Matrix4x4::from_affine_translation(
+            &self.position
+        );
+        self.translation_matrix = translation_inv.inverse().unwrap();
+        self.view_matrix = self.rotation_matrix * self.translation_matrix;
     }
 
     /// Update the light's attitude based on the input change in light 
@@ -332,6 +390,7 @@ impl<S> LightAttitude<S> where S: ScalarFloat {
     fn update(&mut self, delta_attitude: &DeltaAttitude<S>) {
         self.update_orientation_eye(delta_attitude);
         self.update_position_eye(delta_attitude);
+        self.view_matrix = self.rotation_matrix * self.translation_matrix;
     }
 }
 
@@ -357,7 +416,7 @@ impl<S, M> Light<S, M>
     }
 
     /// Update the light's attitude (i.e. position and orientation) in
-    /// world space.
+    /// the light's local coordinate frame.
     #[inline]
     pub fn update_attitude_eye(&mut self, delta_attitude: &DeltaAttitude<S>) {
         self.attitude.update(delta_attitude);
@@ -396,6 +455,24 @@ impl<S, M> Light<S, M>
     pub fn forward_axis(&self) -> Vector3<S> {
         self.attitude.forward.contract()
     }
+
+    /// Get the camera's **vertical y-axis** in camera view space.
+    #[inline]
+    pub fn up_axis_eye(&self) -> Vector3<S> {
+        self.attitude.up_axis_eye()
+    }
+        
+    /// Get the camera's **horizontal x-axis** in camera view space.
+     #[inline]
+    pub fn right_axis_eye(&self) -> Vector3<S> {
+        self.attitude.right_axis_eye()
+    }
+        
+    /// Get the camera's **forward z-axis** in camera view space.
+    #[inline]
+    pub fn forward_axis_eye(&self) -> Vector3<S> {
+        self.attitude.forward_axis_eye()
+    }
         
     /// Get the camera's axis of rotation.
     #[inline]
@@ -403,6 +480,11 @@ impl<S, M> Light<S, M>
         self.attitude.axis.v
     }
 
+    #[inline]
+    pub fn view_matrix(&self) -> &Matrix4x4<S> {
+        &self.attitude.view_matrix
+    }
+ 
     #[inline]
     pub fn model_matrix(&self) -> Matrix4x4<S> {
         Matrix4x4::from_affine_translation(&self.position())
